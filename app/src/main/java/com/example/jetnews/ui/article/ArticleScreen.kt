45d3bde74a01bbc04a8a354c483288a73d0231d1
neobada93 <neo@infozen.co.kr>
2021-07-20 10:08:58 +0900

    First Commit Sample

---
+++ app/src/main/java/com/example/jetnews/ui/article/ArticleScreen.kt
@@ -0,0 +1,274 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.jetnews.ui.article
+
+import android.content.Context
+import android.content.Intent
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.LocalContentColor
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.filled.ThumbUpOffAlt
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetnews.R
+import com.example.jetnews.data.Result
+import com.example.jetnews.data.posts.PostsRepository
+import com.example.jetnews.data.posts.impl.BlockingFakePostsRepository
+import com.example.jetnews.data.posts.impl.post3
+import com.example.jetnews.model.Post
+import com.example.jetnews.ui.components.InsetAwareTopAppBar
+import com.example.jetnews.ui.home.BookmarkButton
+import com.example.jetnews.ui.theme.JetnewsTheme
+import com.example.jetnews.utils.produceUiState
+import com.example.jetnews.utils.supportWideScreen
+import com.google.accompanist.insets.navigationBarsPadding
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+/**
+ * Stateful Article Screen that manages state using [produceUiState]
+ *
+ * @param postId (state) the post to show
+ * @param postsRepository data source for this screen
+ * @param onBack (event) request back navigation
+ */
+@Suppress("DEPRECATION") // allow ViewModelLifecycleScope call
+@Composable
+fun ArticleScreen(
+    postId: String?,
+    postsRepository: PostsRepository,
+    onBack: () -> Unit
+) {
+    val (post) = produceUiState(postsRepository, postId) {
+        getPost(postId)
+    }
+    // TODO: handle errors when the repository is capable of creating them
+    val postData = post.value.data ?: return
+
+    // [collectAsState] will automatically collect a Flow<T> and return a State<T> object that
+    // updates whenever the Flow emits a value. Collection is cancelled when [collectAsState] is
+    // removed from the composition tree.
+    val favorites by postsRepository.observeFavorites().collectAsState(setOf())
+    val isFavorite = favorites.contains(postId)
+
+    // Returns a [CoroutineScope] that is scoped to the lifecycle of [ArticleScreen]. When this
+    // screen is removed from composition, the scope will be cancelled.
+    val coroutineScope = rememberCoroutineScope()
+
+    ArticleScreen(
+        post = postData,
+        onBack = onBack,
+        isFavorite = isFavorite,
+        onToggleFavorite = {
+            coroutineScope.launch { postId?.let { postsRepository.toggleFavorite(postId) } }
+        }
+    )
+}
+
+/**
+ * Stateless Article Screen that displays a single post.
+ *
+ * @param post (state) item to display
+ * @param onBack (event) request navigate back
+ * @param isFavorite (state) is this item currently a favorite
+ * @param onToggleFavorite (event) request that this post toggle it's favorite state
+ */
+@Composable
+fun ArticleScreen(
+    post: Post,
+    onBack: () -> Unit,
+    isFavorite: Boolean,
+    onToggleFavorite: () -> Unit
+) {
+
+    var showDialog by rememberSaveable { mutableStateOf(false) }
+    if (showDialog) {
+        FunctionalityNotAvailablePopup { showDialog = false }
+    }
+
+    Scaffold(
+        topBar = {
+            InsetAwareTopAppBar(
+                title = {
+                    Text(
+                        text = "Published in: ${post.publication?.name}",
+                        style = MaterialTheme.typography.subtitle2,
+                        color = LocalContentColor.current
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = onBack) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(R.string.cd_navigate_up)
+                        )
+                    }
+                }
+            )
+        },
+        bottomBar = {
+            BottomBar(
+                post = post,
+                onUnimplementedAction = { showDialog = true },
+                isFavorite = isFavorite,
+                onToggleFavorite = onToggleFavorite
+            )
+        }
+    ) { innerPadding ->
+        PostContent(
+            post = post,
+            modifier = Modifier
+                // innerPadding takes into account the top and bottom bar
+                .padding(innerPadding)
+                // offset content in landscape mode to account for the navigation bar
+                .navigationBarsPadding(bottom = false)
+                // center content in landscape mode
+                .supportWideScreen()
+        )
+    }
+}
+
+/**
+ * Bottom bar for Article screen
+ *
+ * @param post (state) used in share sheet to share the post
+ * @param onUnimplementedAction (event) called when the user performs an unimplemented action
+ * @param isFavorite (state) if this post is currently a favorite
+ * @param onToggleFavorite (event) request this post toggle it's favorite status
+ */
+@Composable
+private fun BottomBar(
+    post: Post,
+    onUnimplementedAction: () -> Unit,
+    isFavorite: Boolean,
+    onToggleFavorite: () -> Unit
+) {
+    Surface(elevation = 8.dp) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .navigationBarsPadding()
+                .height(56.dp)
+                .fillMaxWidth()
+        ) {
+            IconButton(onClick = onUnimplementedAction) {
+                Icon(
+                    imageVector = Icons.Filled.ThumbUpOffAlt,
+                    contentDescription = stringResource(R.string.cd_add_to_favorites)
+                )
+            }
+            BookmarkButton(
+                isBookmarked = isFavorite,
+                onClick = onToggleFavorite
+            )
+            val context = LocalContext.current
+            IconButton(onClick = { sharePost(post, context) }) {
+                Icon(
+                    imageVector = Icons.Filled.Share,
+                    contentDescription = stringResource(R.string.cd_share)
+                )
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            IconButton(onClick = onUnimplementedAction) {
+                Icon(
+                    painter = painterResource(R.drawable.ic_text_settings),
+                    contentDescription = stringResource(R.string.cd_text_settings)
+                )
+            }
+        }
+    }
+}
+
+/**
+ * Display a popup explaining functionality not available.
+ *
+ * @param onDismiss (event) request the popup be dismissed
+ */
+@Composable
+private fun FunctionalityNotAvailablePopup(onDismiss: () -> Unit) {
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        text = {
+            Text(
+                text = "Functionality not available \uD83D\uDE48",
+                style = MaterialTheme.typography.body2
+            )
+        },
+        confirmButton = {
+            TextButton(onClick = onDismiss) {
+                Text(text = "CLOSE")
+            }
+        }
+    )
+}
+
+/**
+ * Show a share sheet for a post
+ *
+ * @param post to share
+ * @param context Android context to show the share sheet in
+ */
+private fun sharePost(post: Post, context: Context) {
+    val intent = Intent(Intent.ACTION_SEND).apply {
+        type = "text/plain"
+        putExtra(Intent.EXTRA_TITLE, post.title)
+        putExtra(Intent.EXTRA_TEXT, post.url)
+    }
+    context.startActivity(Intent.createChooser(intent, "Share post"))
+}
+
+@Preview("Article screen")
+@Preview("Article screen (dark)", uiMode = UI_MODE_NIGHT_YES)
+@Preview("Article screen (big font)", fontScale = 1.5f)
+@Preview("Article screen (large screen)", device = Devices.PIXEL_C)
+@Composable
+fun PreviewArticle() {
+    JetnewsTheme {
+        val post = runBlocking {
+            (BlockingFakePostsRepository().getPost(post3.id) as Result.Success).data
+        }
+        ArticleScreen(post, {}, false, {})
+    }
+}
